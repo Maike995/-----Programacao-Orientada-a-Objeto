e212709e36f7db03378496c8befd699eb1824e16
Maike995 <105318393+Maike995@users.noreply.github.com>
2022-06-05 21:52:03 -0300

    Update Funcionario.java
    a

--- Aula 05/Sistema/Funcionario.java
+++ Aula 05/Sistema/Funcionario.java
@@ -4,6 +4,12 @@ package sistema;
 
 
 public class Funcionario extends Usuario{
+  
+  public Funcionario(String nome, int senha){
+  this.nome=nome;
+  this.senha=senha;
+  }
+  
 
   public void calcularVenda(int quantidadeDeVenda, double valorUnitario, double listarVenda){
         System.out.println("Relatorio de vendas| Quantidade De Venda: "+quantidadeDeVenda+" | valor unitário: "+valorUnitario+" | listar venda: "+listarVenda);
