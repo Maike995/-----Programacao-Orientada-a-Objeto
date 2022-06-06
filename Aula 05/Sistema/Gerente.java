56920bf69c43330245eedf117bf1041e8f325029
Maike995 <105318393+Maike995@users.noreply.github.com>
2022-06-05 22:01:46 -0300

    Update Gerente.java

--- Aula 05/Sistema/Gerente.java
+++ Aula 05/Sistema/Gerente.java
@@ -6,6 +6,10 @@ package sistema;
 
 public class Gerente extends Funcionario{
 
+  public Gerente(String nome, int senha){
+    super(nome, senha);
+  }
+  
   public void fechar(){
         
         System.out.println("O caixa está feixado");
